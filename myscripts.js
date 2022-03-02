2d787fc4b3b166a704aaf54910799ad5ea720cf9
xiaozhuzai44 <92701340+xiaozhuzai44@users.noreply.github.com>
2022-03-02 01:47:26 -0500

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,14 @@
+let processScroll = () => {
+	let docElem = document.documentElement,
+		docBody = document.body,
+		scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+		scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+		scrollPercent = scrollTop / scrollBottom * 100 + '%';
+		
+	//console.log(scrollTop + '/' + scrollBoom + '/' + scrollPercent);
+	
+	document.getElementById('progress-bar').style.setProperty('--scrollAmount', scrollPercent);
+}
+
+document.addEventListener('scroll', processScroll);
+
